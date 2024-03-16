9eb18fe364529e5a12e77bcaa0f9bba5e49c8cc7
josemavarez11 <josemmr311@gmail.com>
2024-03-15 21:05:21 -0400

    fav and like done at my post

--- src/app/components/containers/my-post/my-post.component.ts
+++ src/app/components/containers/my-post/my-post.component.ts
@@ -4,6 +4,7 @@ import { Storage} from '@angular/fire/storage';
 import { ModalComponent } from '../../others/modal/modal.component';
 import { SwitchService } from 'src/services/switch.service';
 import { GetResult, Preferences } from '@capacitor/preferences';
+import { alert } from 'src/app/utils/alert';
 
 @Component({
   selector: 'app-my-post',
@@ -42,6 +43,7 @@ export class MyPostComponent  implements OnInit {
   modalOpenHandler() {
     this.modalOpen = true;
   }
+
   onFavoriteClick() {
     this.favoriteClicked = !this.favoriteClicked;
   }
@@ -53,4 +55,92 @@ export class MyPostComponent  implements OnInit {
   onEditClick(){
     console.log('edit');
   }
+
+  async likeClick(){
+    const likeIcon = document.getElementById('Glyph');
+
+    if(this.liked){
+      if(likeIcon) likeIcon.style.fill = '#d5cbd9';
+      console.log(`vamos a quitarle el like al targetId: ${this._id}`)
+      try {
+        const response = await fetch('https://fakebook-api-dev-qamc.3.us-1.fl0.io/api/likes/dislike', {
+          method: 'DELETE',
+          headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${this.token.value}`,
+          },
+          body: JSON.stringify({ targetId: this._id })
+        });
+
+        if(response.status !== 200) return alert('Error!', 'Server error removing like', ['OK']);
+
+        return this.liked = !this.liked;
+      } catch (error) {
+        return alert('Error!', 'Unable to remove like', ['OK']);
+      }
+    } else {
+      if(likeIcon) likeIcon.style.fill = '#4D2959';
+      console.log(`vamos a darle like al targetId: ${this._id}`)
+      try {
+        const response = await fetch('https://fakebook-api-dev-qamc.3.us-1.fl0.io/api/likes/like', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${this.token.value}`,
+          },
+          body: JSON.stringify({ targetId: this._id })
+        });
+
+        if(response.status !== 201) return alert('Error!', 'Server error adding like', ['OK']);
+
+        return this.liked = !this.liked;
+      } catch (error) {
+        return alert('Error!', 'Unable to add like', ['OK']);
+      }
+    }
+  }
+
+  async favClick(){
+    const favIcon = document.getElementById('Layer_1');
+    if(this.favorited) {
+      if(favIcon) favIcon.style.fill = '#d5cbd9';
+      console.log(`vamos a quitarle el favorito al post: ${this._id}`);
+      try {
+        const response = await fetch('https://fakebook-api-dev-qamc.3.us-1.fl0.io/api/favorites/remove', {
+          method: 'DELETE',
+           headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${this.token.value}`
+          },
+          body: JSON.stringify({ postId: this._id })
+        });
+
+        if(response.status !== 200) return alert('Error!', 'Server error removing favorite', ['OK']);
+
+        return this.favorited = !this.favorited;
+      } catch (error) {
+        return alert('Error!', 'Unable to remove favorite', ['OK']);
+      }
+    } else {
+      console.log(`vamos a darle favorito al post: ${this._id}`);
+      if(favIcon) favIcon.style.fill = '#4D2959';
+      try {
+        const response = await fetch('https://fakebook-api-dev-qamc.3.us-1.fl0.io/api/favorites/add', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${this.token.value}`
+          },
+          body: JSON.stringify({ postId: this._id })
+        });
+
+        if(response.status !== 201) return alert('Error!', 'Server error adding favorite', ['OK']);
+
+        return this.favorited = !this.favorited;
+      } catch (error) {
+        return alert('Error!', 'Unable to add favorite', ['OK']);
+      }
+    }
+  }
+
 }
